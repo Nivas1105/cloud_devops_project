00dab895842393b266c4de3b4582229a865868cd
Nivas <nivasmarudhavanan@gmail.com>
2025-10-17 22:26:19 -0500

    adding tf files for checking

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+  }
+}
+
+provider "random" {}
+
+resource "random_pet" "name" {
+  length = 2
+}

32ef8979bbee2cfbafb9fa1a331be893adbba274
Nivas <nivasmarudhavanan@gmail.com>
2025-10-17 23:51:39 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-nivas"
     key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
